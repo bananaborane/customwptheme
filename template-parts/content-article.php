c5e112e81526abeb0dc2f88859b522c3bb015f45
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-08-26 00:13:41 -0700

    working on custom comment section

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,3 +1,19 @@
-<?php
-    the_content();
-?>
+    <div class="container">
+        <header class="content-header">
+            <div class="meta mb-3"><span class="date"><?php echo the_date(); ?></span>
+                <span class="tag"><i class="fa fa-tag"></i>tag</span>
+                <?php 
+                    the_tags('<span class="tag"><i class="fa fa-tag"></i> ', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
+                ?>
+                <span class="comment"><a href="#comments"><i class="fa-fa-comment"></i><?php comments_number(); ?></a></span></div>
+        </header>
+        <?php
+            the_content();
+        ?>
+
+        <?php
+            comments_template();
+        ?>
+
+
+    </div>
