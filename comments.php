8de2eca50f44feb96c46665b1f51b34e011d668e
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-08-26 16:50:31 -0700

    comments functional with before and after tags

--- comments.php
+++ comments.php
@@ -38,10 +38,17 @@
 					</div><!-- comments -->
 
 					<hr class="" aria-hidden="true">
-					<div id="respond" class="comment-respond">
-						<h2 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow"
-									id="cancel-comment-reply-link" href="/?p=1#respond" style="display:none;">Cancel
-									reply</a></small></h2>
-					</div><!-- #respond -->
+                    
+                    <?php
+                        if (comments_open()){
+                            comment_form(
+                                array(
+                                    'class_form' => '',
+                                    'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
+                                    'title_reply_after' => '</h2>'
+                                );
+                            );
+                        }
+                    ?>
 
 				</div>
